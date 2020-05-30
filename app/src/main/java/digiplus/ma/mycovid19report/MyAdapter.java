218b99e46791f4ec278c7402dbb25eeb232c8e3a
zelbatmi <zakari_a@icloud.com>
2020-05-30 18:49:24 +0000

    1.0

--- app/src/main/java/digiplus/ma/mycovid19report/MyAdapter.java
+++ app/src/main/java/digiplus/ma/mycovid19report/MyAdapter.java
@@ -2,6 +2,7 @@ package digiplus.ma.mycovid19report;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.drawable.GradientDrawable;
 import android.icu.text.DecimalFormat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,11 +20,14 @@ import org.w3c.dom.Text;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private ArrayList<Cdata> mCdatas;
     private static final String LOCATION_SEPARATOR = " of ";
     private String nt = null;
+    private Context ct = null;
+    private GradientDrawable CasesCircle;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView mRegion, mCases, mDate, mTime, dateView, OFFS;
@@ -37,6 +41,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             dateView = itemView.findViewById(R.id.DateID);
             OFFS = itemView.findViewById(R.id.Region_offsetID);
             nt = itemView.getContext().getString(R.string.near_the);
+            CasesCircle = (GradientDrawable) mCases.getBackground();
+            ct = itemView.getContext();
+
 
         }
     }
@@ -65,6 +72,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.dateView.setText(formattedDate);
         String formattedTime = formatTime(dateObject);
         holder.mTime.setText(formattedTime);
+
+        //colors
+
+        int CasesColor = getCasesColor(currentData.getmCases());
+        CasesCircle.setColor(CasesColor);
+
 
         //split
         String originalLocation = currentData.getmRegion();
@@ -110,4 +123,46 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         DecimalFormat casesFormat = new DecimalFormat("0.0");
         return casesFormat.format(cases);
     }
+
+    //Helper Colors code :
+
+    private int getCasesColor(double cases) {
+        int caseColorResourceId;
+        int caseFloor = (int) Math.floor(cases);
+        switch (caseFloor) {
+            case 0:
+            case 1:
+                caseColorResourceId = R.color.Level1;
+                break;
+            case 2:
+                caseColorResourceId = R.color.Level2;
+                break;
+            case 3:
+                caseColorResourceId = R.color.Level3;
+                break;
+            case 4:
+                caseColorResourceId = R.color.Level4;
+                break;
+            case 5:
+                caseColorResourceId = R.color.Level5;
+                break;
+            case 6:
+                caseColorResourceId = R.color.Level6;
+                break;
+            case 7:
+                caseColorResourceId = R.color.Level7;
+                break;
+            case 8:
+                caseColorResourceId = R.color.Level8;
+                break;
+            case 9:
+                caseColorResourceId = R.color.Level9;
+                break;
+            default:
+                caseColorResourceId = R.color.Level10;
+                break;
+        }
+        return ContextCompat.getColor(ct, caseColorResourceId);
+    }
+
 }
